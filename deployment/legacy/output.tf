66e591288496c7f5fd0ef484976212a932c35641
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-09 14:09:09 +0800

    clean up

--- deployment/legacy/output.tf
+++
@@ -1,9 +0,0 @@
-# output "alb_dns" {
-#   value = module.ecs_services.alb_dns
-# }
-
-
-# resource "local_file" "write_url" {
-#   content  = "ALB_DNS_URL=${module.ecs_services.alb_dns}"
-#   filename = "${path.module}/alb_dns_url.dat"
-# }
