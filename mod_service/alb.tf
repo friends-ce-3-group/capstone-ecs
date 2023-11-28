8f4f81d43b7570aac19e1c39c99427c9474d6a0e
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-28 12:37:54 +0800

    increase healthcheck interval

--- mod_service/alb.tf
+++ mod_service/alb.tf
@@ -7,7 +7,7 @@ resource "aws_alb_target_group" "service" {
 
   health_check {
     healthy_threshold   = "2"
-    interval            = "30"
+    interval            = "120"
     protocol            = "HTTP"
     matcher             = "200"
     timeout             = "3"
