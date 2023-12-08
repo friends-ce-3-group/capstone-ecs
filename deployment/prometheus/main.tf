72ea51e894de5256cbab1534a974097e51c07eb0
Low Chee Meng <lowcheemeng02@gmail.com>
2023-12-08 13:43:26 +0800

    update prom mod

--- deployment/prometheus/main.tf
+++ deployment/prometheus/main.tf
@@ -1,11 +1,11 @@
 data "template_file" "cloudfmn_template" {
   template = file("${path.module}/../../mod_prometheus/prometheus-install.sh.in")
   vars = {
+    AWS_DEFAULT_REGION = var.region
     ECS_CLUSTER_NAME = data.aws_ecs_cluster.ecs_cluster.cluster_name
     ECS_CLUSTER_SECURITY_GROUP = data.aws_security_group.ecs_secgrp.id
     ECS_CLUSTER_SUBNET = element(data.aws_subnets.pvt_subnets.ids, 0)
     ECS_LAUNCH_TYPE = "FARGATE"
-    AWS_DEFAULT_REGION = "us-west-2"
     CREATE_IAM_ROLES = "True"
   }
 }
